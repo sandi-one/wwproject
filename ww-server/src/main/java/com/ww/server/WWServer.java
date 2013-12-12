cdf4bbea747e56b1f2c595f994a2137a05ded9bd
Alexander Babilo <ababilo@issart.com>
2013-12-12 22:09:16 +0700

    [WWServer] Added basic server and sockets implementation

---
+++ ww-server/src/main/java/com/ww/server/WWServer.java
@@ -0,0 +1,24 @@
+package com.ww.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+
+/**
+ *
+ * @author sandy
+ */
+
+public class WWServer {
+    public static final int DEFAULT_WWSERVER_PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        Server wwServer = new Server(DEFAULT_WWSERVER_PORT);
+
+        DefaultSocketHandler socketHandler = new DefaultSocketHandler();
+        socketHandler.setHandler(new DefaultHandler());
+        // set listening mode
+        wwServer.setHandler(socketHandler);
+        wwServer.start();
+        wwServer.join();
+    }
+}
